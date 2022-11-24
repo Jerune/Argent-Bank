c7e1bb9a7905e67bfbe0dd3947b9efeec7683d63
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-11-24 09:54:43 +0100

    feat: Creation Header, Footer, Home, SignIn & UserAccount components
    Static HTML pages are transformed to static components for HeaderNav, Footer, Home, SignIn and UserAccount

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text">Copyright 2022 Argent Bank</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/HeaderNav.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function HeaderNav() {
+  return (
+    <nav className="main-nav">
+      <a className="main-nav-logo" href="./index.html">
+        <img
+          className="main-nav-logo-image"
+          src="./img/argentBankLogo.png"
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </a>
+      <div>
+        <a className="main-nav-item" href="./sign-in.html">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </a>
+      </div>
+    </nav>
+  )
+}
