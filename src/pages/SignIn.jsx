dfa46c7c7f44f1d1d3278af1f1e9b0ebf80b7d25
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-11-24 10:43:18 +0100

    feat: All static data & styles loaded
    All HTML & CSS styles are now loaded and transformed to React components

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -19,12 +19,7 @@ export default function SignIn() {
             <input type="checkbox" id="remember-me" />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          {/* PLACEHOLDER DUE TO STATIC SITE */}
-          <a href="./user.html" className="sign-in-button">
-            Sign In
-          </a>
-          {/* SHOULD BE THE BUTTON BELOW */}
-          {/* <button className="sign-in-button">Sign In</button> */}
+          <button className="sign-in-button">Sign In</button>
         </form>
       </section>
     </main>
