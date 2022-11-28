61b62a795523d35e48e531eaff5d0b4edb890aea
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-11-28 16:12:10 +0100

    feat: Created useFetch hook
    useFetch hook can be used for all currently available ftech requests in the app

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,62 @@
-import React from 'react'
+// @ts-nocheck
+import { useEffect, useState } from 'react'
 
-export default function useFetch() {
+export function useFetch(path, type, data) {
+  const [returnData, setReturnData] = useState({})
+  const [isLoading, setLoading] = useState(true)
+
   const baseURL = 'localhost:3001/api/v1'
+  const fetchURL = `${baseURL}${path}`
 
-  return <div>useFetch</div>
+  const setFetchDataByType = () => {
+    switch (type) {
+      case 'login':
+        return {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: data.email,
+            password: data.password,
+          }),
+        }
+      case 'profile':
+        return {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${data.token}`,
+          },
+        }
+      case 'update':
+        return {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${data.token}`,
+          },
+          body: JSON.stringify({
+            firstName: data.firstName,
+            lastName: data.lastName,
+          }),
+        }
+    }
+  }
+
+  useEffect(() => {
+    if (!fetchURL) return console.log('Incorrect Fetch Request')
+    async function fetchData() {
+      const fetchContent = setFetchDataByType()
+      const response = await fetch(fetchURL, fetchContent)
+      const data = await response.json()
+      setReturnData(data)
+      setLoading(false)
+    }
+
+    setLoading(true)
+    fetchData()
+  }, [fetchURL])
+
+  return [isLoading, returnData]
 }
