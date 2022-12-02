fbdc3e80e1ba39650b57d4eb9491db3e9d487b18
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-12-02 10:23:48 +0100

    feat: 404 Page
    Added 404 page to the routes to have not found page in case users type an unknown string in the url path

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import NotFound from './pages/404'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
@@ -11,9 +12,10 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
